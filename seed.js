b1e60aae8af058e7b70a4d86a0879da06fe8fb92
Sam Narisi <snarisi@gmail.com>
2016-03-16 20:08:58 -0400

    Added seed file

---
+++ seed.js
@@ -0,0 +1,86 @@
+var mongoose = require('mongoose');
+var Item = mongoose.model('Item');
+var User = mongoose.model('User');
+var Cart = mongoose.model('ShoppinCart');
+
+var items = [
+  {
+    name: 'Eggs',
+    cost: 50,
+    category: 'produce'
+  },
+  {
+    name: 'Bread',
+    cost: 500,
+    category: 'produce'
+  },
+  {
+    name: 'Mustard',
+    cost: 2,
+    category: 'produce'
+  },
+  {
+    name: 'Ice Cream',
+    cost: 20,
+    category: 'frozen'
+  },
+  {
+    name: 'Soda',
+    cost: 50,
+    category: 'soda'
+  },
+];
+
+var users = [
+  {
+    name: 'Yustynn',
+    age: 99,
+    budget: 10000,
+    email: 'yus@ty.nn'
+  },
+  {
+    name: 'Sam',
+    age: 99,
+    budget: 10000,
+    email: 's@a.m'
+  },
+  {
+    name: 'Leon',
+    age: 99,
+    budget: 10000,
+    email: 'le@o.n'
+  },
+];
+
+var createdItems;
+var createdUsers;
+
+Item.create(items)
+  .then(function (newItems) {
+    createdItems = newItems;
+    return User.create(users);
+  })
+  .then(function (newUsers) {
+    createdUsers = newUsers;
+  })
+  .then(function () {
+    var carts = [
+      {
+        items: [createdItems[0], createdItems[1]],
+        user: createdUsers[0]
+      },
+      {
+        items: [createdItems[2], createdItems[3]],
+        user: createdUsers[1]
+      },
+      {
+        items: [createdItems[4], createdItems[0]],
+        user: createdUsers[2]
+      },
+    ];
+    return Cart.create(carts);
+  })
+  .then(function () {
+    process.kill(0);
+  })
+  .then(null, console.error.bind(console));
